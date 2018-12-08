9192376b9b2d615685953857b16905a03f0dd5c9
leppsalujyrgen <leppsalujyrgen@gmail.com>
2018-12-08 16:30:26 +0200

    Lisa toit funktsioon töötab.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import askyesno
+from tkinter.messagebox import showinfo
 
 def veateade():
     print("Teie toodet ei leitud... Proovige uuesti.")
@@ -22,6 +24,8 @@ def leia_kaloraaž(list):
 def otsing(otsitav_toit, faili_nimi="export.csv"):
     vasted = {}
     fail = open(faili_nimi)
+    if otsitav_toit == StringVar():
+        return None
     for rida in fail:
         rida_listina = rida.split(",")
         failist_toit = rida_listina[0].strip("\"")
@@ -83,81 +87,89 @@ def valikute_töötlemine(valikud):
         veateade()
         return None
 
-def uustoit():
-    uus = input("3. Toit puudub andmebaasist. Soovite lisada andmebaasi uue toidu?(jah/ei): ")
-    if uus == "jah":
-        uustoit = input("Sisestage uue toidu nimi (sinna järele soovitatavalt ka kirjeldus): ").lower().capitalize()
-        uuskalor = float(input("Sisestage toidu kaloraaz 100 grammi kohta: "))+0.0000001
-        f = open("export.csv","a")
-        f.write("\n"+'"'+uustoit+'"'+","+str(uuskalor))
-        print("Toit lisatud andmebaasi.")
-        f.close()
+def uustoit(toit, kalor):
+    f = open("export.csv","a")
+    f.write("\n"+'"'+uustoit+'"'+","+str(uuskalor))
+    print("Toit lisatud andmebaasi.")
+    f.close()
+
+def andmete_sisestamine(sisend1, sisend2):
+    toit = sisend1.capitalize()
+    try:
+        kogus = round(float(sisend2), 2)
+    except:
+        showinfo("Teade", "Sisend on vigane. Kontrollige, et kaloraaži lahtrisse on sisestatud ainult arv.")
+        return
+    tekst = "Kas sisestan faili järgmise kirje: \n" + str(toit) + "\n" + str(kogus) + " kcal saja grammi kohta"
+    vastus = askyesno("Kinnitus", tekst)
+    if vastus == "yes":
+        uustoit(toit, kalor)
+        showinfo("Teade", "Andmebaasi on täiendatud.")
     else:
-        return None        
-    
-def p(event):
-    global funktsioon
-    return funktsioon.get()
-    
-    
-    
-def main():
-    root = Tk()
-    #Akna nimi ja suurus
-    root.title("Kalori luger")
-    root.geometry("290x180")
-    #Akna erinevad tab'id ehk leheküljed
-    leheküljed = ttk.Notebook(root)
-    lehekülg1 = ttk.Frame(leheküljed)
-    lehekülg2 = ttk.Frame(leheküljed)
-    leheküljed.add(lehekülg1, text="Kalkulaator")
-    leheküljed.add(lehekülg2, text="Lisa toit")
-    leheküljed.pack(expand=1, fill="both")
-    
-    #!!! ESIMENE LEHT !!!
-    #Raamid
-    raam_lahtrid = Frame(lehekülg1)
-    raam_nupp = Frame(lehekülg1)
-    raam_lahtrid.pack(side=TOP, pady=(30, 10))
-    raam_nupp.pack(side=TOP)
-    #Sildid
-    silt1 = Label(raam_lahtrid, text="Toiduaine: ")
-    silt2 = Label(raam_lahtrid, text="Kogus (g): ")
-    silt1.grid(row=3, column=0, pady=1, sticky=E)
-    silt2.grid(row=4, column=0, pady=1, sticky=E)
-    #Teksti alad
-    toit = Entry(raam_lahtrid)
-    kogus = Entry(raam_lahtrid)
-    toit.grid(row=3, column=1)
-    kogus.grid(row=4, column=1)
-    #Nupud
-    sisesta_nupp = Button(raam_nupp, text="Enter")
-    sisesta_nupp.grid(row=6, column=3, pady=5, sticky="W")
-    
-    #!!! TEINE LEHT !!!
-    #Raamid
-    raam_uustoit = Frame(lehekülg2)
-    raam_uuskaloraaž = Frame(lehekülg2)
-    raam_nupp = Frame(lehekülg2)
-    raam_uustoit.pack(side=TOP, pady=(10,10))
-    raam_uuskaloraaž.pack(side=TOP, pady=(0, 10))
-    raam_nupp.pack(side=TOP, pady=(5, 10))
-    #Uus toiduaine
-    silt1 = Label(raam_uustoit, text="Uus toiduaine: ")
-    silt1.pack(side=TOP)
-    toidu_nimi = Entry(raam_uustoit)
-    toidu_nimi.pack(side=TOP)
-    #Kaloraaž
-    silt2 = Label(raam_uuskaloraaž, text="Kaloraaž (100g kohta): ")
-    silt2.pack(side=TOP)
-    kogus = Entry(raam_uuskaloraaž)
-    kogus.pack(side=TOP)
-    #Nupp
-    uus_toit_nupp = Button(raam_nupp, text="Sisesta")
-    uus_toit_nupp.grid(row=5, column= 1, sticky=W)
-    
-    root.mainloop()
-
-main()
+        showinfo("Teade", "Toitu ei sisestatud.")
 
 
+root = Tk()
+
+#Akna nimi ja suurus
+root.title("Kalori luger")
+root.geometry("290x180")
+#Akna erinevad tab'id ehk leheküljed
+leheküljed = ttk.Notebook(root)
+lehekülg1 = ttk.Frame(leheküljed)
+lehekülg2 = ttk.Frame(leheküljed)
+leheküljed.add(lehekülg1, text="Kalkulaator")
+leheküljed.add(lehekülg2, text="Lisa toit")
+leheküljed.pack(expand=1, fill="both")
+    
+#!!! ESIMENE LEHT !!!
+#Raamid
+raam_lahtrid = Frame(lehekülg1)
+raam_nupp = Frame(lehekülg1)
+raam_lahtrid.pack(side=TOP, pady=(30, 10))
+raam_nupp.pack(side=TOP)
+#Sildid
+silt1 = Label(raam_lahtrid, text="Toiduaine: ")
+silt2 = Label(raam_lahtrid, text="Kogus (g): ")
+silt1.grid(row=3, column=0, pady=1, sticky=E)
+silt2.grid(row=4, column=0, pady=1, sticky=E)
+#Teksti alad
+global sisend
+sisend = StringVar()
+toit = ttk.Combobox(raam_lahtrid, textvariable=sisend, width=17, height=10)
+
+if sisend.get() != "":
+    toit["values"] = vaste_listina(otsing(sisend.get()))
+toit.grid(row=3, column=1)
+kogus = Entry(raam_lahtrid)
+kogus.grid(row=4, column=1)
+#Nupud
+sisesta_nupp = Button(raam_nupp, text="Enter",
+                          command=lambda: andmete_sisestamine(toit.get(), kogus.get()))
+sisesta_nupp.grid(row=6, column=3, pady=5, sticky="W")
+
+#!!! TEINE LEHT !!!
+#Raamid
+raam_uustoit = Frame(lehekülg2)
+raam_uuskaloraaž = Frame(lehekülg2)
+raam_nupp = Frame(lehekülg2)
+raam_uustoit.pack(side=TOP, pady=(10,10))
+raam_uuskaloraaž.pack(side=TOP, pady=(0, 10))
+raam_nupp.pack(side=TOP, pady=(5, 10))
+#Uus toiduaine
+uus_toit = StringVar()
+silt1 = Label(raam_uustoit, text="Uus toiduaine: ")
+silt1.pack(side=TOP)
+toidu_nimi = Entry(raam_uustoit, textvariable=uus_toit)
+toidu_nimi.pack(side=TOP)
+#Kaloraaž
+kaloraaž = StringVar()
+silt2 = Label(raam_uuskaloraaž, text="Kaloraaž (100g kohta): ")
+silt2.pack(side=TOP)
+kogus = Entry(raam_uuskaloraaž, textvariable=kaloraaž)
+kogus.pack(side=TOP)
+#Nupp
+uus_toit_nupp = Button(raam_nupp, text="Sisesta", command=lambda: andmete_sisestamine(uus_toit.get(), kaloraaž.get()))
+uus_toit_nupp.grid(row=5, column= 1, sticky=W)
+  
+root.mainloop()
